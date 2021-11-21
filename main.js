7e79db2edfb1c50295fac4d411135b6d4932be37
Raf <raf@brounber.com>
2021-11-21 15:54:26 +1100

    added added JS

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
+/*==================== Overall Constant Created ====================*/
+
 const displayMain = document.querySelector('.display__main');
 const displayResult = document.querySelector('.display__results');
-const numbers = docuemnt.querySelector('.button__number');
-const operators = document.querySelector('.button__operation');
+const numbers = document.querySelectorAll('.button__number');
+const operators = document.querySelectorAll('.button__operation');
 const equals = document.querySelector('.button__equal');
-const clear = document.querySelector('button__last-entity-clear');
+const clear = document.querySelector('.button__last-entity-clear');
 
-let displayMain = '';
-let displayResult = null;
-let lastOperation = '';
+
+/*==================== Changeable Veriable ====================*/
+let disMain = '';
+let disResult = null;
+let prevOperation = '';
 let haveDot = false; 
+
+// created an eventlistener for the numbers to be displayed and also made a rule for the "dot"
+numbers.forEach(number => { 
+  number.addEventListener('click', (e) => { 
+    if (e.target.innerText === '.' && !haveDot) {
+      haveDot = true;
+    } else if (e.target.innerText === '.' && haveDot) {
+      return;
+    }
+    disMain += e.target.innerText;
+    displayMain.innerText = disMain;
+  })
+});
+
+//created an event listener for the operations buttons, making a rule where the operation symbols will not show if there isn't a number already entered 
+
+
+operators.forEach( operations => {
+  operations.addEventListener('click', (e) => {
+    if (!disMain) results; 
+    haveDot = false;
+    const operationName = e.target.innerText;
+    if(disMain && prevOperation) {
+      mathOperations();
+    } else {
+      result = parseFloat(disMain);
+    }
+    console.log(results)
+  })
+})
 
